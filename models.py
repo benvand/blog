b35f25a09781cd953ae60e35142c67553906b5b9
benvand <benjamin_vandersteen@hotmail.com>
2013-12-13 10:17:02 +0000

    update

--- models.py
+++ models.py
@@ -1,19 +1,20 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-#from justdifferentsites.models import Site
 from django.contrib.sites.models import Site
-
 #User class is:
 User = get_user_model()
 # Create your models here.
 
 class Post(models.Model):
     title =             models.CharField(max_length=255)
-    content =           models.TextField() 
-    picture =           models.ImageField(upload_to='blog/static/post_pics', blank=True)
-    created =           models.TimeField(auto_now_add=True)
-    modified =          models.TimeField(auto_now=True)
+    content =           models.TextField()
+    created =           models.DateTimeField(auto_now_add=True)
+    modified =          models.DateTimeField(auto_now=True)
     userid =            models.ForeignKey(User, related_name='userblogpost')
+    PostPicture = None
+
+    def __unicode__(self):
+        return self.title
 
     class Meta():
         ordering = ('created',)
@@ -28,18 +29,29 @@ class Comment(models.Model):
     userid =            models.ForeignKey(User, related_name='userblogcomment')
     postid =            models.ForeignKey('Post')
 
+    def __unicode__(self):
+        return self.userid + ': ' + self.content[:20]
+
     class Meta():
         ordering = ('created',)
         get_latest_by = ('created',)
         verbose_name = 'Blog Comment'
         verbose_name_plural = 'Blog Comments'
 
+class PostPicture(models.Model):
+    picture =           models.ImageField(upload_to='blog/post_pics', blank=True)
+    postid =            models.ForeignKey('Post')
+
+
 
 class BlogOptions(models.Model):
     site = models.OneToOneField(Site)
     title = models.CharField(max_length=200,blank=True)
     intro = models.TextField(max_length=1000,blank=True)
-    backgroundImage = models.ImageField(upload_to='static', blank=True)
+    backgroundImage = models.ImageField(upload_to='blog/', blank=True)
+
+    def __unicode__(self):
+        return self.site.name.capitalize() + " Blog Page Options"
 
     class Meta:
         verbose_name = 'Blog Page Options'
